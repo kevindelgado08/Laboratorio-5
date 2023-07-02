228ea329aeffaddea70be6bc076c829e7c5f3699
andresnv12 <andres.chavesvargas@ucr.ac.cr>
2023-07-02 16:35:10 -0600

    Se realizaron cambios en el archivo sensorMovimientos.ino. Se incluyeron los archivos .csv para realizar el entrenamiento mediante edge impulse y .zip generado al realizar el deployment.

--- sensorMovimientos/sensorMovimientos.ino
+++ sensorMovimientos/sensorMovimientos.ino
@@ -4,31 +4,29 @@ void setup() {
   // Se inicializa el puerto serial
   Serial.begin(9600);
   while (!Serial);
-  Serial.println("Inicio de Conexión");
-
+  
   if (!IMU.begin()) {
     Serial.println("Fallo al iniciar el giroscopio!");
     while (1);
   }
-  Serial.print("Rango de Pruebas del Giroscopio = ");
-  Serial.print(IMU.gyroscopeSampleRate());
-  Serial.println(" Hz");
-  Serial.println();
-  Serial.println("Giroscopio en grados/segundos");
-  Serial.println("X\tY\tZ");
+  
 }
-
+int prueba = 0;
 void loop() {
   float x, y, z;
 
   if (IMU.gyroscopeAvailable()) {
     IMU.readGyroscope(x, y, z);
-
-    Serial.print(x);
-    Serial.print('\t');
-    Serial.print(y);
-    Serial.print('\t');
-    Serial.println(z);
+    if (prueba < 2000) {
+      Serial.print(prueba);
+      Serial.print('\t');
+      Serial.print(x);
+      Serial.print('\t');
+      Serial.print(y);
+      Serial.print('\t');
+      Serial.println(z);
+      prueba = prueba + 1;
+    }
   }
 }
 
